d348fb47c40cc974b0218cfd524021fb250dfad7
hq <hq.email@qq.com>
2022-04-15 20:27:42 +0800

    deploygit

---
+++ deploy-k8s.sh
@@ -0,0 +1,18 @@
+# !/usr/bin/env sh
+# 生成静态文件
+
+rm -rf docs/.vuepress/dist
+
+sed -i "s/\\/blog\\//\\//g" docs/.vuepress/config.ts
+
+npm run build
+
+git add .
+
+# shellcheck disable=SC2046
+git commit -m 'deploy'git
+
+# 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
+git push origin master https://gitee.com/hq-study/blog.git
+# git push -f git@github.com:h-qing/blogs.git
+
