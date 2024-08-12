0918133dd241080b3449da410766d9151ba7e5e2
hq <hq.email@qq.com>
2024-08-12 14:57:26 +0800

    add

--- deploy-k8s.sh
+++ deploy-k8s.sh
@@ -1,17 +1,17 @@
-# !/usr/bin/env sh
-# 生成静态文件
-
-rm -rf docs/.vuepress/dist
-
-sed -i "s#BASE='/blog/'#BASE='/'#g" docs/.vuepress/config.ts
-
-npm run build
-
-git add .
-
-# shellcheck disable=SC2046
-git commit -m 'deploy'git
-
-# 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
-git push -f  https://gitee.com/hq-study/blog.git master:master
-
+## !/usr/bin/env sh
+## 生成静态文件
+#
+#rm -rf docs/.vuepress/dist
+#
+#sed -i "s#BASE='/blog/'#BASE='/'#g" docs/.vuepress/config.ts
+#
+#npm run build
+#
+#git add .
+#
+## shellcheck disable=SC2046
+#git commit -m 'deploy'git
+#
+## 如果发布到 https://<USERNAME>.github.io  填写你刚刚创建的仓库地址
+#git push -f  https://gitee.com/hq-study/blog.git master:master
+#
